52b667736be08f511694eab74676cc0b161bc071
ViktorSlavchev <viki.slavchev05@smg.bg>
2025-08-14 11:28:47 +0300

    wrong questions mobile-friendly

--- src/pages/WrongQuestions.jsx
+++ src/pages/WrongQuestions.jsx
@@ -8,16 +8,58 @@ import InfoBox from "../ui/InfoBox";
 import WrongQuestion from "../features/mistaken/WrongQuestion";
 import QuestionHolder from "../features/exam/QuestionHolder";
 import Spinner from "../ui/Spinner";
+import mediaQueries from "../styles/breakpoints";
 import { belTopics, litTopics } from "../utils/topics";
 import { useMistakenQuestions, useDeleteMistakenQuestion } from "../features/mistaken/useMistakenQuestions";
+
+const MainContainer = styled.div`
+	display: flex;
+	gap: 3.2rem;
+	width: 100%;
+	
+	${mediaQueries.tablet} {
+		gap: 2.4rem;
+	}
+	
+	${mediaQueries.mobileOnly} {
+		flex-direction: column;
+		gap: 1.6rem;
+	}
+`;
+
+const ContentColumn = styled.div`
+	flex: 2;
+	min-width: 0;
+	
+	${mediaQueries.mobileOnly} {
+		order: 2;
+		flex: 1;
+	}
+`;
 
 const RightColumn = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 1.6rem;
 	align-self: flex-start;
-	/* position: sticky; */
+	flex: 1;
+	min-width: 0;
+	position: sticky;
 	top: 2rem;
+	
+	${mediaQueries.tabletOnly} {
+		gap: 1.2rem;
+		position: static;
+	}
+	
+	${mediaQueries.mobileOnly} {
+		order: 1;
+		position: static;
+		flex: none;
+		gap: 1.2rem;
+		margin-bottom: 0.8rem;
+		width: 100%;
+	}
 `;
 
 const TopicsBox = styled(InfoBox)`
@@ -25,6 +67,18 @@ const TopicsBox = styled(InfoBox)`
 	flex-direction: column;
 	gap: 1.2rem;
 	padding: 2rem;
+	width: 100%;
+	
+	${mediaQueries.tablet} {
+		padding: 1.6rem;
+		gap: 1rem;
+	}
+	
+	${mediaQueries.mobileOnly} {
+		padding: 1.2rem;
+		gap: 1rem;
+		width: 100%;
+	}
 `;
 
 const TopicButton = styled.button`
@@ -38,16 +92,60 @@ const TopicButton = styled.button`
 	text-align: left;
 	font-size: 1.4rem;
 	transition: all 0.2s;
+	min-height: 4rem;
+	display: flex;
+	align-items: center;
+	width: 100%;
 
 	&:hover {
 		background: var(--color-brand-50);
 		border-color: var(--color-brand);
 		color: var(--color-brand);
 	}
+	
+	${mediaQueries.mobileOnly} {
+		padding: 0.8rem 1rem;
+		font-size: 1.3rem;
+		min-height: 3.6rem;
+		justify-content: center;
+		text-align: center;
+	}
+	
+	${mediaQueries.smallMobile} {
+		padding: 0.8rem 1rem;
+		font-size: 1.2rem;
+		min-height: 3.6rem;
+	}
+`;
+
+const CategoryButtonContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.8rem;
+	
+	${mediaQueries.mobileOnly} {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 1rem;
+		width: 100%;
+	}
+	
+	${mediaQueries.smallMobile} {
+		grid-template-columns: 1fr;
+		gap: 0.8rem;
+	}
 `;
 
 const TopicSection = styled.div`
 	margin-bottom: 3.2rem;
+	
+	${mediaQueries.tablet} {
+		margin-bottom: 2.4rem;
+	}
+	
+	${mediaQueries.mobileOnly} {
+		margin-bottom: 2rem;
+	}
 `;
 
 const TopicTitle = styled(Text)`
@@ -56,6 +154,40 @@ const TopicTitle = styled(Text)`
 	margin-bottom: 1.6rem;
 	padding: 1.2rem 0;
 	border-bottom: 2px solid var(--color-border);
+	
+	${mediaQueries.tablet} {
+		font-size: 1.7rem;
+		margin-bottom: 1.4rem;
+		padding: 1rem 0;
+	}
+	
+	${mediaQueries.mobileOnly} {
+		font-size: 1.6rem;
+		margin-bottom: 1.2rem;
+		padding: 0.8rem 0;
+	}
+`;
+
+const ResponsiveHeading = styled(Heading)`
+	${mediaQueries.mobileOnly} {
+		font-size: 2.4rem;
+		margin-bottom: 1.6rem;
+	}
+	
+	${mediaQueries.smallMobile} {
+		font-size: 2.2rem;
+		margin-bottom: 1.2rem;
+	}
+`;
+
+const SidebarTitle = styled(Text)`
+	${mediaQueries.mobileOnly} {
+		font-size: 1.4rem !important;
+	}
+	
+	${mediaQueries.smallMobile} {
+		font-size: 1.3rem !important;
+	}
 `;
 
 function WrongQuestions() {
@@ -149,61 +281,67 @@ function WrongQuestions() {
 
 	return (
 		<>
-			<Heading>Предишни грешки</Heading>
-			<Row $gap="3.2rem" style={{ width: "100%" }}>
-				<QuestionHolder style={{ flex: 2, minWidth: "0" }}>
-					{!mistakenQuestions || mistakenQuestions.length === 0 ? (
-						<InfoBox>
-							<Text style={{ textAlign: "center", padding: "2rem" }}>Няма регистрирани грешни отговори</Text>
-						</InfoBox>
-					) : Object.keys(questionsGrouped).length === 0 ? (
-						<InfoBox>
-							<Text style={{ textAlign: "center", padding: "2rem" }}>Няма грешни отговори за категория &ldquo;{activeCategory}&rdquo;</Text>
-						</InfoBox>
-					) : (
-						currentTopics
-							.filter((topic) => questionsGrouped[topic])
-							.map((topic) => (
-								<TopicSection key={topic} ref={(el) => (sectionRefs.current[topic] = el)}>
-									<TopicTitle>{topic}</TopicTitle>
-									{questionsGrouped[topic].map((question, index) => (
-										<WrongQuestion key={question._id} question={question} num={index + 1} onDelete={handleDeleteQuestion} isDeleting={isDeleting} />
-									))}
-								</TopicSection>
-							))
-					)}
-				</QuestionHolder>
+			<ResponsiveHeading>Предишни грешки</ResponsiveHeading>
+			<MainContainer>
+				<ContentColumn>
+					<QuestionHolder>
+						{!mistakenQuestions || mistakenQuestions.length === 0 ? (
+							<InfoBox>
+								<Text style={{ textAlign: "center", padding: "2rem" }}>Няма регистрирани грешни отговори</Text>
+							</InfoBox>
+						) : Object.keys(questionsGrouped).length === 0 ? (
+							<InfoBox>
+								<Text style={{ textAlign: "center", padding: "2rem" }}>Няма грешни отговори за категория &ldquo;{activeCategory}&rdquo;</Text>
+							</InfoBox>
+						) : (
+							currentTopics
+								.filter((topic) => questionsGrouped[topic])
+								.map((topic) => (
+									<TopicSection key={topic} ref={(el) => (sectionRefs.current[topic] = el)}>
+										<TopicTitle>{topic}</TopicTitle>
+										{questionsGrouped[topic].map((question, index) => (
+											<WrongQuestion key={question._id} question={question} num={index + 1} onDelete={handleDeleteQuestion} isDeleting={isDeleting} />
+										))}
+									</TopicSection>
+								))
+						)}
+					</QuestionHolder>
+				</ContentColumn>
 
-				<RightColumn style={{ flex: 1, minWidth: "0" }}>
+				<RightColumn>
 					{availableCategories.length > 0 && (
 						<TopicsBox>
-							<Text $weight="bold" $size="1.6rem" style={{ marginBottom: "0.8rem" }}>
+							<SidebarTitle $weight="bold" $size="1.6rem" style={{ marginBottom: "0.8rem" }}>
 								Категории
-							</Text>
-							{availableCategories.map((category) => (
-								<TopicButton key={category} $active={activeCategory === category} onClick={() => setActiveCategory(category)}>
-									{category}
-								</TopicButton>
-							))}
+							</SidebarTitle>
+							<CategoryButtonContainer>
+								{availableCategories.map((category) => (
+									<TopicButton key={category} $active={activeCategory === category} onClick={() => setActiveCategory(category)}>
+										{category}
+									</TopicButton>
+								))}
+							</CategoryButtonContainer>
 						</TopicsBox>
 					)}
 
 					{Object.keys(questionsGrouped).length > 0 && (
 						<TopicsBox>
-							<Text $weight="bold" $size="1.6rem" style={{ marginBottom: "0.8rem" }}>
+							<SidebarTitle $weight="bold" $size="1.6rem" style={{ marginBottom: "0.8rem" }}>
 								Теми в {activeCategory}
-							</Text>
-							{currentTopics
-								.filter((topic) => questionsGrouped[topic])
-								.map((topic) => (
-									<TopicButton key={topic} onClick={() => scrollToTopic(topic)}>
-										{topic} ({questionsGrouped[topic]?.length || 0})
-									</TopicButton>
-								))}
+							</SidebarTitle>
+							<CategoryButtonContainer>
+								{currentTopics
+									.filter((topic) => questionsGrouped[topic])
+									.map((topic) => (
+										<TopicButton key={topic} onClick={() => scrollToTopic(topic)}>
+											{topic} ({questionsGrouped[topic]?.length || 0})
+										</TopicButton>
+									))}
+							</CategoryButtonContainer>
 						</TopicsBox>
 					)}
 				</RightColumn>
-			</Row>
+			</MainContainer>
 		</>
 	);
 }
